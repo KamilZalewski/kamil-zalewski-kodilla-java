aeaff428224568821c7d74fb2e7b4635829a021b
KamilZalewski <kamilzalewski89@gmail.com>
2020-05-07 10:36:38 +0200

    calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator(15,89);
+
+        int resultOfSum = calculator.sum();
+        if(resultOfSum == (104)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        int resultOfSubtract = calculator.subtract();
+        if(resultOfSubtract == (-74)) {
+            System.out.println("test Ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,41 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+            return a;
+        }
+        public int getB() {
+            return b;
+        }
+
+        @Override
+        public String toString() {
+            return ("Result of the equasion is: ");
+        }
+
+        public int sum() {
+            return a + b;
+        }
+        public int subtract() {
+            return a - b;
+        }
+    }
+    class Application{
+
+        public static void main(String[] args) {
+            Calculator calculator = new Calculator(15,89);
+            int resultOfSum = calculator.sum();
+            System.out.println("Sum result is: " + resultOfSum);
+            int resultOfSubtraction = calculator.subtract();
+            System.out.println("Subtraction result is: " + resultOfSubtraction);
+        }
+    }
+
