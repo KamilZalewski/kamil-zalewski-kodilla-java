35526851010b23c225f795573fd661449189337a
KamilZalewski <kamilzalewski89@gmail.com>
2020-05-17 11:54:42 +0200

    task 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,38 +1,21 @@
+
 package com.kodilla.stream;
-import java.lang.Object;
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        System.out.println("Making text more beatiful");
-        System.out.println(poemBeautifier.beautify("more beatiful sentence", (text -> "ABC" + text + "ABC")));
-        System.out.println(poemBeautifier.beautify("this sentence is the most beatiful sentence in the world", (text -> text.toUpperCase())));
-        System.out.println(poemBeautifier.beautify("HOW MUCH THIS SENTENCE IS BEATIFUL", (text -> text.toLowerCase())));
-        System.out.println(poemBeautifier.beautify("I LOVE WORKING WITH CODE", (text -> text.replace("CODE", "JAVA"))));
-
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-
+    Forum forum = new Forum();
+        Map<Integer,ForumUser> mapOfUsers = forum.getUsersList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getDateOfBirth().getYear() <= 2000)
+                .filter(forumUser -> forumUser.getPublishedPosts() >=1)
+                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
+        for(Map.Entry<Integer, ForumUser> entry :mapOfUsers.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue() + "\n");
+        }
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream.forumuser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Forum {
+   List<ForumUser> usersList = new ArrayList<>();
+
+    public Forum(){
+       usersList.add(new ForumUser(1,"Kamil Zalewski", 'M',1989,6,16,10));
+       usersList.add(new ForumUser(2,"Paula Zalewska", 'K',1990,7,31,5));
+       usersList.add(new ForumUser(3,"Adam Zalewski", 'M',1983,5,17,3));
+       usersList.add(new ForumUser(4,"Justyna Zalewska - Rejdak", 'K',1975,5,10,15));
+       usersList.add(new ForumUser(5,"Przemysław Rejdak", 'M',1975,2,18,0));
+       usersList.add(new ForumUser(6,"Waldemar Szabat", 'M',1965,7,14,5));
+       usersList.add(new ForumUser(7,"Tymoteusz Rejdak", 'M',2007,9,21,50));
+       usersList.add(new ForumUser(8,"Zuzanna Rejdak", 'M',2003,8,14,22));
+       usersList.add(new ForumUser(9,"Alicja Szabat", 'K',1999,2,22,4));
+       usersList.add(new ForumUser(10,"Andrzej Swirski", 'M',1989,4,8,7));
+   }
+    public List<ForumUser> getUsersList() {
+        return usersList;
+    }
+}
