86ec394bc5013a239a4ed8d09500411318fd8152
KamilZalewski <kamilzalewski89@gmail.com>
2020-05-25 15:25:41 +0200

    corrected task 8.2 and 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -3,7 +3,8 @@ package com.kodilla.exception.test;
 public class ExceptionHandling {
     public static void main(String[] args) {
         SecondChallenge secondChallenge = new SecondChallenge();
-        try { secondChallenge.probablyIWillThrowException(3,2);
+        try {
+            secondChallenge.probablyIWillThrowException(3,2);
         } catch (Exception e) {
             System.out.println("Errorek: " + e);
         } finally {
