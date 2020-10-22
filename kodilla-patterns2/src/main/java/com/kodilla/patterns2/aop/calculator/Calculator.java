88e41a0ad63463ccc3a6b0299e0750d8e10adc85
Kamil Zalewski <kamilzalewski89@gmail.com>
2020-10-22 16:49:34 +0200

    task 24.3 aspect

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/aop/calculator/Calculator.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/aop/calculator/Calculator.java
@@ -1,8 +1,12 @@
 package com.kodilla.patterns2.aop.calculator;
 
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
+@EnableAspectJAutoProxy
 public class Calculator {
     public double add(double x, double y) {
         return x + y;
@@ -17,9 +21,19 @@ public class Calculator {
     }
 
     public double div(double x, double y) {
-        if (y == 0){
+        if (y == 0) {
             throw new ArithmeticException("Divide by zero!");
         }
         return x / y;
+    }
+
+    public BigDecimal factorial(BigDecimal n) {
+        if (n.compareTo(BigDecimal.ONE) > 0) {
+            return n.multiply(factorial(n.subtract(BigDecimal.ONE)));
+        } else if (n.equals(BigDecimal.ONE) || n.equals(BigDecimal.ZERO)) {
+            return BigDecimal.ONE;
+        } else {
+            throw new ArithmeticException("Factorial argument is negative!");
+        }
     }
 }
